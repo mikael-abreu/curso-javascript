67f84f52ddc524a7d22a48239fb32099a2fd593c
Mikael Abreu <mikaelgtcp@gmail.com>
2022-05-23 23:41:32 -0300

    aulas js

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`a velocidade do seu carro é ${vel} KM/h`)
+if (vel > 60) {//condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija com cuidade! Use o cinto!`)
